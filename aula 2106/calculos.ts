af365fc0bc97be386a4382e247f029e71e98e0ad
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-21 21:48:50 -0300

    funções ts

---
+++ aula 2106/calculos.ts
@@ -0,0 +1,22 @@
+var nome:string
+nome = "cleito"
+
+ function soma(n1:number, n2:number):number{
+     return n1 + n2
+ }
+
+ 
+
+function subtrair(n1:number, n2:number):number{
+    return (n1-n2)
+}
+
+function dividir(n1:number, n2:number):number{
+    return (n1 / n2)
+}
+
+function multiplicar(n1:number, n2:number):number{
+    return (n1*n2)
+}
+
+export {soma, subtrair, dividir, multiplicar}
